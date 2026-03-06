36dbed745b79a940503a56e6388008ffa0c6fa48
Eren <muhammeterenmente2@gmail.com>
2026-03-07 00:22:08 +0300

    Proje için geliştirme ortamı yapıldı.

---
+++ src/test/java/com/eren/social_media_analysis/SocialMediaAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eren.social_media_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
